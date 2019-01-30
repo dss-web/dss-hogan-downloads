69fae266a8c3116cd78ebee0d65ab66564063284
Robert Sæther <robert@dekode.no>
2019-01-30 13:48:37 +0100

    Added more error checks since customer got a 500 when viewing the download-section

--- assets/template.php
+++ assets/template.php
@@ -22,23 +22,27 @@ if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof DSS_Downloads ) ) {
 		<li>
 			<?php
 			$title                          = $download['title'] ?: esc_html__( 'Download the file', 'hogan-dss-downloads' );
-			$file_name                      = $download['file']['filename'];
-			$attachment_file_path           = get_attached_file( $download['file']['id'] );
+			$file                           = ( isset( $download['file'] ) ? $download['file'] : false );
+			$file_url                       = ( ( $file && isset( $file['url'] ) ) ? $file['url'] : false );
+			$file_name                      = ( ( $file && isset( $file['filename'] ) ) ? $file['filename'] : false );
+			$file_mime_type                 = ( ( $file && isset( $file['mime_type'] ) ) ? $file['mime_type'] : false );
+			$file_id                        = ( ( $file && isset( $file['id'] ) ) ? $file['id'] : false );
+			$attachment_file_path           = get_attached_file( $file_id );
 			$attachment_file_size_formatted = '';
-			if ( false !== $attachment_file_path ) :
+			if ( false !== $attachment_file_path && file_exists( $attachment_file_path ) && is_readable( $attachment_file_path ) ) :
 				$attachment_file_size           = filesize( $attachment_file_path );
 				$attachment_file_size_formatted = size_format( $attachment_file_size );
 			endif;
 			$max_chars = 50;
 			if ( strlen( $file_name ) > $max_chars ) {
-				$mime_type_array   = explode( '/', $download['file']['mime_type'] );
+				$mime_type_array   = explode( '/', $file_mime_type );
 				$mime_type         = $mime_type_array[ count( $mime_type_array ) - 1 ];
 				$file_name_chopped = substr( $file_name, 0, ( $max_chars - 3 ) ) . '...' . $mime_type;
 			} else {
 				$file_name_chopped = $file_name;
 			}
 			?>
-			<p><a href="<?php echo $download['file']['url']; ?>" class="title" title="<?php echo $file_name; ?>"
+			<p><a href="<?php echo $file_url; ?>" class="title" title="<?php echo $file_name; ?>"
 				  download><?php echo $title; ?></a><br>
 				<span class="file-info"><?php echo $file_name_chopped . ' | ' . $attachment_file_size_formatted; ?></span>
 			</p>
